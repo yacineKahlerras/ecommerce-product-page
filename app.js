5d85107884af4597081bffcbf76561ac954c6459
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-08-21 23:08:42 +0100

    ok

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 /** vars */
 const sidebar = document.querySelector(".sidebar-container");
 const toggleSideMenuBtns = [...document.querySelectorAll(".toggle-sidebar")];
+const cartBtn = document.querySelector(".cart-btn");
+const cartElement = document.querySelector(".cart");
 
+/** event listeners */
 toggleSideMenuBtns.forEach((b) =>
   b.addEventListener("click", () => {
     sidebar.classList.toggle("show-sidebar");
   })
 );
+cartBtn.addEventListener("click", () => {
+  cartElement.classList.toggle("show-cart");
+});
