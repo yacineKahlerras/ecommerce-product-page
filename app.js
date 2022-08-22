88f4ccf09be456dd34fea23613493110641f2676
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-08-22 16:40:52 +0100

    ok

--- app.js
+++ app.js
@@ -107,3 +107,80 @@ plusBtn.addEventListener("click", () => editProductAmount(true));
 
 // add to cart
 addToCartBtn.addEventListener("click", addToCart);
+
+/**********************************
+ **********************************
+ *          image galery
+ *********************************
+ *********************************
+ * */
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const galery = [...document.querySelector(".galery").children];
+let currentImgIndex = 0;
+
+const slide = (forward) => {
+  let translateAmount = 0;
+  if (forward && currentImgIndex < galery.length - 1) {
+    currentImgIndex++;
+    prevBtn.classList.remove("hide-slide-btn");
+    if (currentImgIndex == galery.length - 1)
+      nextBtn.classList.add("hide-slide-btn");
+  } else if (!forward && currentImgIndex > 0) {
+    currentImgIndex--;
+    nextBtn.classList.remove("hide-slide-btn");
+    if (currentImgIndex == 0) prevBtn.classList.add("hide-slide-btn");
+  }
+  translateAmount = currentImgIndex * -100;
+  galery.forEach((img) => {
+    img.style.transform = `translateX(${translateAmount}%)`;
+  });
+};
+
+prevBtn.addEventListener("click", () => slide(false));
+nextBtn.addEventListener("click", () => slide(true));
+
+/** SWIPING */
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      slide(true);
+    } else {
+      slide(false);
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
+
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
